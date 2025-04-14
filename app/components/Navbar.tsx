bff218edc8e3a4565aa35d2489f5ca4322c2780a
Giddy-K <gideonkipamet@gmail.com>
2025-04-14 12:17:20 +0300

    Minor changes in navbar.tsx

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
+import Image from "next/image";
+import { auth, signOut, signIn } from "@/auth";
+import Google from "next-auth/providers/google";
 
-const Navbar = () => {
+
+
+
+const Navbar = async () => {
+  const session = await auth();
   return (
-    <div>Navbar</div>
-  )
-}
+    <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
+      <nav className="flex justify-between items-center">
+        <Link href="/">
+          <Image
+            src="/icon.png"
+            alt="Mzinga Hub"
+            width={100}
+            height={100}
+            border-radius={50}
+          />
+        </Link>
+        <div className="flex items-center gap-5 text-black">
+          {session && session?.user ? (
+            <>
+              <Link href="/account/create">
+                <span>Create Account</span>
+              </Link>
 
-export default Navbar
+              <button onClick={signOut}>
+                <span>LogOut</span>
+              </button>
+
+              <Link href={`/user/${session?.id}`}>
+                <span>{session?.user?.name}</span>
+              </Link>
+            </>
+          ) : (
+            <button onClick={async () => {await signIn(provider?: Google)}}>
+              <span>Access Account</span>
+            </button>
+          )}
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
