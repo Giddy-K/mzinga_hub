d99e11949739dce500d77e783fd612f2460d1917
Giddy-K <gideonkipamet@gmail.com>
2025-04-14 14:38:59 +0300

    Finished implementing authentication with google

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,12 +3,13 @@ import React from "react";
 import Image from "next/image";
 import { auth, signOut, signIn } from "@/auth";
 import Google from "next-auth/providers/google";
-
-
-
+import { redirect } from "next/dist/server/api-utils";
 
 const Navbar = async () => {
+  const GOOGLE_CLIENT_ID = process.env.AUTH_GOOGLE_ID!;
+  const GOOGLE_CLIENT_SECRET = process.env.AUTH_GOOGLE_SECRET!;
   const session = await auth();
+
   return (
     <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
       <nav className="flex justify-between items-center">
@@ -28,18 +29,28 @@ const Navbar = async () => {
                 <span>Create Account</span>
               </Link>
 
-              <button onClick={signOut}>
-                <span>LogOut</span>
-              </button>
+              <form
+                action={async () => {
+                  "use server";
+                  await signOut({ redirectTo: "/" });
+                }}
+              >
+                <button type="submit">LogOut</button>
+              </form>
 
               <Link href={`/user/${session?.id}`}>
                 <span>{session?.user?.name}</span>
               </Link>
             </>
           ) : (
-            <button onClick={async () => {await signIn(provider?: Google)}}>
-              <span>Access Account</span>
-            </button>
+            <form
+              action={async () => {
+                "use server";
+                await signIn("google");
+              }}
+            >
+              <button type="submit">Access Account</button>
+            </form>
           )}
         </div>
       </nav>
