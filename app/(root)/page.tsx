5499452ecf1e51e3807f5e2f01d4149af77f0fa0
Giddy-K <gideonkipamet@gmail.com>
2025-04-20 04:41:34 +0300

    Added to do comments

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,7 +3,7 @@
 // text-white-100/80 = .text-14-normal
 // pattern, bg-primary = .pink_container
 // tag-tri, bg-secondary, font-work-sans = .tag
-//  = 
+// text-black-100 = no-result
 
 export default function Home() {
   return (
