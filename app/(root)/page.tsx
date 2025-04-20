58c5547dc94cae8f548fd94390fa648cbc56e5e6
Giddy-K <gideonkipamet@gmail.com>
2025-04-21 00:49:38 +0300

    Adds hover effect to startup cards
    Improves the startup card component by adding a hover effect.
    
    This change introduces a `hover:border-primary` class to the `.startup-card` element, enhancing user interaction and visual feedback when hovering over startup cards.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -11,7 +11,7 @@
 // !important, shadow-100, font-work-sans = .login
 // placeholder:text-black-100 = .search-input
 // !important = .search-btn
-// shadow-200 = .startup-card
+// shadow-200, hover:border-primary = .startup-card
 
 export default function Home() {
   return (
