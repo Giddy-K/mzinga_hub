eb2013aa691e24c12675423766b0d8abf3c49a87
Giddy-K <gideonkipamet@gmail.com>
2025-04-20 04:49:26 +0300

    Minor changes

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -5,7 +5,7 @@
 // tag-tri, bg-secondary, font-work-sans = .tag
 // text-black-100 = .no-result
 // bg-primary, shadow-100 = .profile_card
-
+// shadow-100 = .profile_title
 export default function Home() {
   return (
     <>
