2001253aa914eca4ac77176827eeb2371359db22
Giddy-K <gideonkipamet@gmail.com>
2025-05-03 13:16:43 +0300

    Implemented redirect on login

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { SessionProvider } from "next-auth/react";
+import RedirectOnLogin from "./components/RedirectOnLogin";
 
 const workSans = localFont({
   src: [
@@ -71,6 +72,7 @@ export default function RootLayout({
         className={workSans.variable}
       >
         <SessionProvider>
+        <RedirectOnLogin />
           {children}
         </SessionProvider>
       </body>
