61760107a640a537d4e5e9c0e90c7d6ad1ca3f2a
Giddy-K <gideonkipamet@gmail.com>
2025-05-03 15:39:16 +0300

    Added unauthorized draft page

---
+++ app/unauthorized/page.tsx
@@ -0,0 +1,9 @@
+export default function UnauthorizedPage() {
+    return (
+      <main>
+        <h1>Access Denied</h1>
+        <p>You do not have permission to access this page.</p>
+      </main>
+    )
+  }
+  
