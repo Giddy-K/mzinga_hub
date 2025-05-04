ea0978d47a79ac09a0de2da0ece23f82a85c5f74
Giddy-K <gideonkipamet@gmail.com>
2025-05-04 23:42:54 +0300

    Added admin get-user routes

---
+++ app/api/admin/get-users/route.ts
@@ -0,0 +1,9 @@
+// File: app/api/admin/get-users/route.ts
+import { admin } from "@/lib/firebase-admin";
+
+export async function GET() {
+  const snapshot = await admin.firestore().collection("users").get();
+  const users = snapshot.docs.map((doc) => doc.data());
+
+  return Response.json({ users });
+}
