8b90e7cfd82f6dfffe17897d2a14a9b11e26ceee
Giddy-K <gideonkipamet@gmail.com>
2025-05-05 15:07:00 +0300

    Added Redirect on login

---
+++ app/account/RedirectOnLogin.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { useSession } from "next-auth/react";
+import { useEffect } from "react";
+import { checkUserRoleAndRedirect } from "@/lib/checkRoleAndRedirect";
+import { useRouter } from "next/navigation";
+
+export default function RedirectOnLogin() {
+  const { data: session } = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (session?.user?.email) {
+      checkUserRoleAndRedirect(session.user.email, router);
+    }
+  }, [session]);
+
+  return null;
+}
