d76826b97cb4cee4557fb20baa333d6491149704
Giddy-K <gideonkipamet@gmail.com>
2025-05-05 15:07:00 +0300

    Added redirects root page

---
+++ app/account/redirect/page.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { useSession } from "next-auth/react";
+import { useEffect } from "react";
+import { checkUserRoleAndRedirect } from "@/lib/checkRoleAndRedirect";
+import { useRouter } from "next/navigation";
+import FullScreenLoader from "@/app/components/FullScreenLoader";
+
+export default function RedirectPage() {
+  const { data: session, status } = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRedirect = async () => {
+      if (status === "loading") return;
+
+      if (session?.user?.email) {
+        await checkUserRoleAndRedirect(session.user.email, router);
+      } else {
+        router.push("/unauthorized");
+      }
+    };
+
+    handleRedirect();
+  }, [session, status]);
+
+  return <FullScreenLoader />;
+}
