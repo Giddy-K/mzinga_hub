97422dcf98393483a3cff4bd562bb08d418e1264
Giddy-K <gideonkipamet@gmail.com>
2025-05-12 20:24:00 +0300

    Minor changes to delete apiary action

--- app/actions/deleteApiaryAction.ts
+++ app/actions/deleteApiaryAction.ts
@@ -1,9 +1,11 @@
-'use server';
+"use server";
 
 import { deleteApiaryById } from "@/lib/firebase/deleteApiary";
 import { redirect } from "next/navigation";
 
-export async function deleteApiaryAction(id: string) {
+export async function deleteApiaryAction(formData: FormData) {
+  const id = formData.get("id") as string;
+  if (!id) throw new Error("Missing apiary ID");
   await deleteApiaryById(id);
   redirect("/apiaries/dashboard");
 }
