a0152a7b0fa98b8d46dee410b2529d6495b9d890
Giddy-K <gideonkipamet@gmail.com>
2025-05-12 08:19:32 +0300

    Added format dates utils

---
+++ lib/utils/formatDate.ts
@@ -0,0 +1,8 @@
+
+export function formatDate(dateString: string) {
+  return new Date(dateString).toLocaleDateString("en-KE", {
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+  });
+}
