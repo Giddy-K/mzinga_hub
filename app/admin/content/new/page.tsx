62ea4f1030e1e3088f2d85560a23a906f51e65df
Giddy-K <gideonkipamet@gmail.com>
2025-05-19 02:49:32 +0300

    Added this page

---
+++ app/admin/content/new/page.tsx
@@ -0,0 +1,13 @@
+// /app/admin/content/new.tsx
+
+import ContentForm from "@/app/components/ContentForm";
+
+
+export default function NewContentPage() {
+  return (
+    <section className="min-h-screen p-10 bg-gray-50">
+      <h1 className="text-2xl font-bold mb-6">Create New Content</h1>
+      <ContentForm />
+    </section>
+  );
+}
