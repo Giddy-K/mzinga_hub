8408ed4a6cc736a2b3dd73014353bbd4ee485316
Giddy-K <gideonkipamet@gmail.com>
2025-05-30 15:13:04 +0300

    Minor changes

--- lib/firebase-admin.ts
+++ lib/firebase-admin.ts
@@ -1,15 +1,28 @@
-// lib/firebase-admin.ts
-import admin from "firebase-admin";
+// import admin from "firebase-admin";
 
-const serviceAccount = JSON.parse(
-  process.env.FIREBASE_SERVICE_ACCOUNT_KEY as string
-);
+// const serviceAccount = JSON.parse(
+//   process.env.FIREBASE_SERVICE_ACCOUNT_KEY as string
+// );
 
-if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://mzingahub-68de6-default-rtdb.firebaseio.com"
+// if (!admin.apps.length) {
+//   admin.initializeApp({
+//     credential: admin.credential.cert(serviceAccount),
+//     databaseURL: "https://mzingahub-68de6-default-rtdb.firebaseio.com"
+//   });
+// }
+
+// export { admin };
+
+// !Updated code
+import { initializeApp, cert, getApps } from "firebase-admin/app";
+import { getDatabase } from "firebase-admin/database";
+
+// Only run once in the server environment
+if (!getApps().length) {
+  initializeApp({
+    credential: cert(JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY!)),
+    databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
   });
 }
 
-export { admin };
+export const adminDB = getDatabase();
