4a095c2ddbed42b76f70a68ca7bdf4c080e2ca42
Giddy-K <gideonkipamet@gmail.com>
2025-06-05 01:13:44 +0300

    Deleted junk lines

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,16 +1,4 @@
-// app/api/auth/[...nextauth]/route.ts
-
-// import NextAuth from "next-auth";
-// import GoogleProvider from "next-auth/providers/google";
-// import CredentialsProvider from "next-auth/providers/credentials";
-// import { doc, getDoc, setDoc } from "firebase/firestore";
-// import { db } from "@/lib/firebase";
 import { handlers } from "@/auth";
-
-// import type { User } from "next-auth";
-// import type { Session } from "next-auth";
-// import type { JWT } from "next-auth/jwt";
-
 
 // ✅ Correctly wrapped request handlers
 export const { GET, POST } = handlers;
