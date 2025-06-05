35bcb4f3335e4767643c16d245f58f7d18ec12aa
Giddy-K <gideonkipamet@gmail.com>
2025-06-06 02:37:17 +0300

    Added middleware

---
+++ middleware.ts
@@ -0,0 +1,22 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import { getToken } from "next-auth/jwt";
+
+export async function middleware(req: NextRequest) {
+  const token = await getToken({ req, secret: process.env.AUTH_SECRET });
+  const { pathname } = req.nextUrl;
+
+  if (!token && pathname.startsWith("/admin")) {
+    return NextResponse.redirect(new URL("/account", req.url));
+  }
+
+  if (pathname.startsWith("/admin") && token?.role !== "admin") {
+    return NextResponse.redirect(new URL("/unauthorized", req.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: [],
+};
