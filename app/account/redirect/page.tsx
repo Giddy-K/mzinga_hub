c83a4fde42c17755c09e2b5b3e073766e64ffe08
Giddy-K <gideonkipamet@gmail.com>
2025-06-06 02:56:15 +0300

    More changes

--- app/account/redirect/page.tsx
+++ app/account/redirect/page.tsx
@@ -23,28 +23,3 @@ export default function RedirectPage() {
 
   return <FullScreenLoader />;
 }
-
-// // app/account/redirect/page.tsx
-// import { auth } from "@/auth";
-// import { redirect } from "next/navigation";
-
-// export default async function RedirectPage() {
-//   const session = await auth();
-
-//   if (!session || !session.user?.role) {
-//     redirect("/unauthorized");
-//   }
-
-//   // Avoid redirecting in a loop
-//   const role = session.user.role;
-
-//   if (role === "admin") {
-//     redirect("/admin/dashboard");
-//   }
-
-//   if (role === "user") {
-//     redirect("/");
-//   }
-
-//   redirect("/unauthorized");
-// }
