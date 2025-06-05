9913269553ffa8b9751a6ab7da70e376a8b484bb
Giddy-K <gideonkipamet@gmail.com>
2025-06-06 00:29:49 +0300

    Minor change

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,6 +1,2 @@
-// lib/auth.ts (or just auth.ts at root)
-
-import NextAuth from "next-auth";
-import { authOptions } from "@/lib/authOptions";
-
-export const { auth } = NextAuth(authOptions);
+// lib/auth.ts
+export { auth } from "@/auth";
